1a6a332c84db9c70a9e180a08a5760909e9c7a9c
Hannu Siikonen <hannu.siikonen@gmail.com>
2020-10-28 15:41:00 +0200

    Multiple updates on pu tools

--- pileup/tools/dt/lxplus/puhistos.sh
+++ pileup/tools/dt/lxplus/puhistos.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 
-FOLDERS=(B2/ C/ D/ E/ Fe/ Fl/ G/ H/)
-TRIGGERS=(HLT_PFJet40 HLT_PFJet60 HLT_PFJet80 HLT_PFJet140 HLT_PFJet200 HLT_PFJet260 HLT_PFJet320 HLT_PFJet400 HLT_PFJet450)
+FOLDERS=(B C D E Fe Fl G H)
+TRIGGERS=(HLT_PFJet40 HLT_PFJet60 HLT_PFJet80 HLT_PFJet140 HLT_PFJet200 HLT_PFJet260 HLT_PFJet320 HLT_PFJet400 HLT_PFJet450) # HLT_PFJet500)
 
-CERTIFICATE=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
+CERTIFICATE=/afs/cern.ch/user/h/hsiikone/work/certificates/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt
+NORMTAG=/afs/cern.ch/user/h/hsiikone/work/Normtags/normtag_PHYSICS.json
+
 PILEUP=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt
-NORMTAG=/afs/cern.ch/user/h/hsiikone/work/Normtags/normtag_DATACERT.json
 MAXPU=80
+XSECS=(69200 80000)
 
-for loc in ${FOLDERS[@]}; do 
-    cd $loc
-
+for loc in ${FOLDERS[@]}; do
     echo "Starting loop"
-    for trg in ${TRIGGERS[@]}; do
-        echo "Trigger "${trg}
-        TAG=clumis_${trg}
-        brilcalc lumi --normtag $NORMTAG -i clumis.json -b "STABLE BEAMS" --hltpath "${trg}_v*" --byls --minBiasXsec 69200 -o ${TAG}.csv
+    for XSEC in ${XSECS[@]}; do
+        TARGET=puhists/${loc}
+        if [[ "$XSEC" -eq "80000" ]]; then
+            TARGET=puhists80/${loc}
+        fi
+        mkdir -p ${TARGET}
+
+        cd ${loc}z
+        echo "Trigger HLT_ZeroBias"
+        TAG=clumis_ZB
+        brilcalc lumi --normtag $NORMTAG -i clumis.json -b "STABLE BEAMS" --hltpath "HLT_ZeroBias_v*" --byls --minBiasXsec $XSEC -o ${TAG}.csv
         wait
         pileupReCalc_HLTpaths.py -i ${TAG}.csv --inputLumiJSON $PILEUP -o ${TAG}_pileup.txt --runperiod Run2
         wait
-        pileupCalc.py -i clumis.json --inputLumiJSON ${TAG}_pileup.txt  --calcMode true --minBiasXsec 69200 --maxPileupBin ${MAXPU} --numPileupBins ${MAXPU} ${trg}.root
+        pileupCalc.py -i clumis.json --inputLumiJSON ${TAG}_pileup.txt --calcMode true --minBiasXsec $XSEC --maxPileupBin ${MAXPU} --numPileupBins ${MAXPU} ../${TARGET}/HLT_PFJet0.root
         wait
         rm ${TAG}*
         wait
+        cd ../$loc
+        for trg in ${TRIGGERS[@]}; do
+            echo "Trigger "${trg}
+            TAG=clumis_${trg}
+            brilcalc lumi --normtag $NORMTAG -i clumis.json -b "STABLE BEAMS" --hltpath "${trg}_v*" --byls --minBiasXsec $XSEC -o ${TAG}.csv
+            wait
+            pileupReCalc_HLTpaths.py -i ${TAG}.csv --inputLumiJSON $PILEUP -o ${TAG}_pileup.txt --runperiod Run2
+            wait
+            pileupCalc.py -i clumis.json --inputLumiJSON ${TAG}_pileup.txt --calcMode true --minBiasXsec $XSEC --maxPileupBin ${MAXPU} --numPileupBins ${MAXPU} ../${TARGET}/${trg}.root
+            wait
+            rm ${TAG}*
+            wait
+        done
+        cd ..
     done
 
     echo "Finished! folder "$loc
-    cd ..
 done
 
 exit 0
--- pileup/tools/dt/normalize.C
+++ pileup/tools/dt/normalize.C
@@ -2,16 +2,19 @@
 #include "TH1D.h"
 #include "TROOT.h"
 
-void normalize() {
-  vector<string> tags = {"500", "450", "400", "320", "260", "200", "140", "80", "60", "40", "0"};
-//  vector<string> tags = {"450", "400", "320", "260", "200", "140", "80", "60", "40", "0"};
+const vector<string> tags = {"500", "450", "400", "320", "260", "200", "140", "80", "60", "40", "0"};
+const vector<string> tags16 = {"450", "400", "320", "260", "200", "140", "80", "60", "40", "0"};
+
+void normalize(const bool is16 = false) {
   TFile f("pileup_DT.root","RECREATE");
   TFile g("pileup_DTu.root","READ");
 
+  const auto &tgs = is16 ? tags16 : tags;
+
   double scale = 1.0;
-  for (unsigned i = 0; i < tags.size(); ++i) {
+  for (unsigned i = 0; i < tgs.size(); ++i) {
     TString name;
-    name.Form("jt%s", tags[i].c_str());
+    name.Form("jt%s", tgs[i].c_str());
     g.cd();
     TH1D* dhandle = dynamic_cast<TH1D*>(gROOT->FindObject(name.Data()));
     f.cd();
